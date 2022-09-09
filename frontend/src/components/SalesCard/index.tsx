e7986ad2f12bff6760b4a09b782ea4e713ce63d5
Guilherme Gonçalves <guilhermexneto@gmail.com>
2022-09-09 20:36:42 -0300

    Other components

---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,84 @@
+
+import NotificationButton from '../NotificationButton'
+import './styles.css'
+
+function SalesCard() {
+    return (
+        <div className="dsmeta-card">
+            <h2 className="dsmeta-sales-title">Vendas</h2>
+            <div>
+                <div className="dsmeta-form-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+                <div className="dsmeta-form-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+            </div>
+
+            <div>
+                <table className="dsmeta-sales-table">
+                    <thead>
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show576">Data</th>
+                            <th>Vendedor</th>
+                            <th className="show992">Ligações</th>
+                            <th className="show992">Vendas</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th className="show992">#341</th>
+                            <th className="show576">13/09/2022</th>
+                            <th>Giovana Vasques</th>
+                            <th className="show992">15</th>
+                            <th className="show992">11</th>
+                            <th>R$ 105.200,00</th>
+                            <th>
+                                <div className="dsmeta-red-btn-container">
+                                   <NotificationButton />
+                                </div>
+
+                            </th>
+                        </tr>
+
+                        <tr>
+                            <th className="show992">#341</th>
+                            <th className="show576">13/09/2022</th>
+                            <th>Gabriela Vasques</th>
+                            <th className="show992">15</th>
+                            <th className="show992">11</th>
+                            <th>R$ 105.200,00</th>
+                            <th>
+                                <div className="dsmeta-red-btn-container">
+                                 <NotificationButton />
+                                </div>
+
+                            </th>
+                        </tr>
+
+                        <tr>
+                            <th className="show992">#341</th>
+                            <th className="show576">13/09/2022</th>
+                            <th>Guilherme Gonçalves</th>
+                            <th className="show992">15</th>
+                            <th className="show992">11</th>
+                            <th>R$ 105.200,00</th>
+                            <th>
+                                <div className="dsmeta-red-btn-container">
+                                  <NotificationButton />
+                                </div>
+
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    )
+}
+
+export default SalesCard
